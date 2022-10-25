f6957d2a1ecebafe4931ed719a8da15f7aaf406a
SchulzCode <116190857+SchulzCode@users.noreply.github.com>
2022-10-26 01:11:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TwoStrings test = new TwoStrings();
+
+        System.out.println(test.arrayStringsAreEqual(new String[]{"h" + "allo"}, new String[]{"ha" + "llo"}));
+    }
+}
---
+++ src/TwoStrings.java
@@ -0,0 +1,8 @@
+public class TwoStrings {
+
+        public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+            return (String.join("", word1).equals(String.join("",word2)));
+
+        }
+}
